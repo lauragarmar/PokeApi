5934ebaed90f857e7731dc68962d78e6982799ec
Laura García <lauragarmar94@gmail.com>
2022-01-17 12:04:14 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+for (let x = 1; x < 8; x++) {
+     for (let y = 1; y < 8; y++) {
+        let randomX=Math.floor(Math.random()*x)
+        let randomY=Math.floor(Math.random()*y)
+
+        console.log(randomX)
+        console.log(randomY)
+     }
+    }
